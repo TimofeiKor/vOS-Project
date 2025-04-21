2763cd1a2f43810b82be1d611c830f1689c9fd09
Latte <koryhalovtimofei@gmail.com>
2025-04-21 22:17:20 +0300

    [21.04.2025] Colored update! vOS build 34:     changes:         - added colors (!!!)         - added boot

--- calc.py
+++ calc.py
@@ -8,10 +8,37 @@ def question():
     char = input("input char: ")
     if char == '+':
         plus()
+    elif char == '-':
+        minus()
+    elif char == '*':
+        multiply()
+    elif char == '/':
+        divide()
 
 def plus():
     x = int(input("input num1: "))
     y = int(input("input num2: "))
     res = x + y
+    print("result: ", res)
+    cmd = input("input 'exit' for closing app... ")
+
+def minus():
+    x = int(input("input num1: "))
+    y = int(input("input num2: "))
+    res = x - y
+    print("result: ", res)
+    cmd = input("input 'exit' for closing app... ")
+
+def divide():
+    x = int(input("input num1: "))
+    y = int(input("input num2: "))
+    res = x / y
+    print("result: ", res)
+    cmd = input("input 'exit' for closing app... ")
+
+def multiply():
+    x = int(input("input num1: "))
+    y = int(input("input num2: "))
+    res = x * y
     print("result: ", res)
     cmd = input("input 'exit' for closing app... ")
--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import calc
 import os
+import time
+from colorama import init, Fore
 
-commands = ["help", "info"]
-apps = ["calc"]
-cmd = input("root/py/> ")
+init(autoreset=True)
 
 def clear():
     os.system('cls')
 
+def boot():
+    clear()
+    print(Fore.YELLOW + "config files [py]...")
+    time.sleep(1)
+    print(Fore.GREEN + "opening files [py]...")
+    time.sleep(0.5)
+    print(Fore.BLUE + "re-config files [py]...")
+    time.sleep(0.5)
+    print(Fore.CYAN + "booting py...")
+    time.sleep(3)
+    clear()
+
+    print(Fore.LIGHTGREEN_EX + "config files [vos]...")
+    time.sleep(1)
+    print(Fore.LIGHTYELLOW_EX + "opening files [vos]...")
+    time.sleep(0.5)
+    print(Fore.LIGHTCYAN_EX + "re-config files [vos]...")
+    time.sleep(0.5)
+    print(Fore.LIGHTMAGENTA_EX + "installing vos...")
+    time.sleep(2)
+    os.system('cls')
+boot()
+
+commands = ["vos", "info"]
+apps = ["calc"]
+cmd = input("root/py/> ")
+
+
 def infoLong():
-    print("vOS build 17")
+    print("vOS build 34")
     print("Powered by Python")
     print("Author: @TimofeiKor(GitHub)")
 
 def infoShort():
-    print("vOS build 17")
+    print(Fore.LIGHTBLUE_EX + "vOS build 19")
 
 def againcmd():
     global cmd
@@ -28,6 +56,9 @@ def helpOpen():
         helpMenu()
 
 def helpMenu():
+        clear()
+        print(Fore.LIGHTMAGENTA_EX + "opening vos...")
+        time.sleep(3)
         clear()
         infoShort()
         print("==========MENU==========")
